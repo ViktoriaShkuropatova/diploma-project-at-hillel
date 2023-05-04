5c420461da6c5d994608a402a00a07e4229bab50
ViktoriaShkuropatova <116377127+ViktoriaShkuropatova@users.noreply.github.com>
2023-05-04 16:15:36 +0300

    added about.html page

--- app/js/script.js
+++ app/js/script.js
@@ -5,3 +5,31 @@ menu__btn.addEventListener('click', function(){
     menu__list.classList.toggle('menu__list_open');
     menu__btn.classList.toggle('menu__btn_active');
 })
+
+$('.js-team-slider').slick({
+    autoplay: true,
+    autoplaySpeed: 3500,
+    arrows: false,
+    dots: true,
+    dotsClass: 'team__dots'
+});
+
+$('.js-achievements-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    dots: true,
+    dotsClass: 'achievements__dots',
+    responsive: [
+        {
+            breakpoint: 678,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+
+            }
+        }
+    ]
+});
